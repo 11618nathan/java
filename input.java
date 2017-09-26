4e9d4545d623e783b64ee5d24a45028306572601
Baek Gi Chan <11618nathan@gmail.com>
2017-09-26 21:48:00 +0900

    Add files via upload

---
+++ input.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class input 
+{
+	public static void main(String[] args) 
+	{
+		// 입력
+		Scanner scan =  new Scanner(System.in);
+		
+		// 첫 번째 문자
+		char x  = scan.nextLine().charAt(0);
+		
+		// 문자열 입력
+		String s = scan.nextLine();
+	}
+}
