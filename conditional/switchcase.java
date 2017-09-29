dc033aa7929867738a5cfa2389fe12813b49e9b5
Baek Gi Chan <11618nathan@gmail.com>
2017-09-29 23:56:07 +0900

    Update switchcase.java

--- conditional/switchcase.java
+++ conditional/switchcase.java
@@ -1,11 +1,13 @@
 
 public class switchcase 
 {
-	x = 1;
+	x = 0;
 	switch (x)
 	{
-	case 1: System.out.println("1"); break;
-	case 2: System.out.println("2"); break;
+	case 1: System.out.println("1"); 
+			break;
+	case 2: System.out.println("2"); 
+			break;
 	case 3: System.out.println("3"); break;
 	default: System.out.println("0"); break;
 	
