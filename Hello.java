385b16d5ad3a811557ddd7e476eb0936aba91b18
Baek Gi Chan <11618nathan@gmail.com>
2017-09-30 19:12:07 +0900

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,11 @@
+
+// 클래스 - 필드 or 메소드를 포함 블록
+public class Hello 
+{
+	// 메소드 - 실행문 블록
+	// main - entry point 먼저 실행
+	public static void main(String[] args)
+	{
+		System.out.println("Hello!");		
+	}
+}
