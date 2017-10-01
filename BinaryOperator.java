8b9eed476a1ffad4027e81a80f143fbfdad6d9ef
Baek Gi Chan <11618nathan@gmail.com>
2017-10-01 21:00:25 +0900

    Add files via upload

---
+++ BinaryOperator.java
@@ -0,0 +1,33 @@
+/*
+package operator;
+
+public class BinaryOperator 
+{
+	public static void main(String[] args)
+	{
+		int x = 11000;
+		int y = 618;
+		int result1, result2, result3, result4, result5 = 0;
+		
+		// +연산자
+		result1 = x + y; 
+		System.out.println("x + y = " + result1);
+		
+		// -연산자
+		result2 = x - y;
+		System.out.println("x - y = " + result2);
+		
+		// *연산자
+		result3 = x * y;
+		System.out.println("x * y = " + result3);
+		
+		// /연산자
+		result4 = x / y;
+		System.out.println("x / y = " + result4);
+		
+		// %연산자 - 나머지
+		result5 = x % y;
+		System.out.println("x % y = " + result5);
+	}
+}
+*/
