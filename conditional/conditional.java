f4ee25f14975e4101c3118b815f91f26bf999416
Baek Gi Chan <11618nathan@gmail.com>
2017-10-22 18:47:37 +0900

    Update conditional.java

--- conditional/conditional.java
+++ conditional/conditional.java
@@ -3,17 +3,26 @@ public class conditional
 {
 	public static void main(String[] args) 
 	{
-		if (true)
-		{
-			
-		}
-		else if
-		{
-			
-		}
-		else
-		{
-			
-		}
+		int num = 11618;
+		 
+		 // if - 조건식에 실행 여부 결정
+		 if(num>=11618)
+		 {
+			 System.out.println("if문 실행");
+		 }
+		 
+		 // if - else if - else
+		 if(num > 11618)
+		 {
+			 System.out.println("if문 실행");
+		 }
+		 else if(num == 11618)
+		 {
+			 System.out.println("if else문 실행");
+		 }
+		 else
+		 {
+			 System.out.println("else문 실행");
+		 }
 	}
 }
