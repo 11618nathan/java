57551266291d1fd21e53ed09e1969fa5b57ffc16
Baek Gi Chan <11618nathan@gmail.com>
2019-02-24 13:11:13 +0900

    Upload

---
+++ Capsule2/src/CapMain.java
@@ -0,0 +1,56 @@
+import java.util.Random;
+
+public class CapMain {
+
+	public static void main(String[] args) {
+		
+		Marin.attUp();
+		
+		Marin m1 =  new Marin(); 
+		m1.showUpgrade();		// 7: 0 : 6
+		
+		Marin m2 = new Marin();
+		m2.showUpgrade(); //  공격력 ?? // 7 : 0 : 6
+	
+		
+/*		Marin m1 =  new Marin(); 
+		m1.attUp();
+		m1.showUpgrade();		// 7: 0 : 6
+		
+		Marin m2 = new Marin();
+		m2.showUpgrade(); //  공격력 ?? // 6 : 0 : 6
+		m1.showUpgrade(); // // 7: 0 : 6
+	*/	
+		
+		
+		
+		/*Random r =  new Random();
+		int value = r.nextInt() % 45 +1;
+		
+		
+		// Math.abs 메서드 사용 ?? 
+		// ->  Math객체 없이 호출하여 사용 !! 
+		// ->  abs메서드는 static으로 선언된 메서드 !! 
+		// ->  객체없이 클래스 이름으로 접근 사용이 가능 !! 
+		System.out.println( Math.abs( value ) );
+		*/
+		
+		
+		
+		
+		//Order os1 =  new Order();
+		//Order os2 =  new Order();
+		//Order os3 =  new Order();
+		
+		
+		
+		
+		
+		/*Pride p1 =  new Pride(1111);
+		Pride p2 =  new Pride(2222);
+		Pride p3 =  new Pride(3333); 
+		*/
+		
+	
+	}
+}
