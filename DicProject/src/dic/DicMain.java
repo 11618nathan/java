c1bf6873ccc02690336746ed2fc75069e89c49ef
Baek Gi Chan <11618nathan@gmail.com>
2019-02-26 18:28:42 +0900

    Upload

---
+++ DicProject/src/dic/DicMain.java
@@ -0,0 +1,33 @@
+package 상속3;
+
+// 전화 !!
+class phone
+{
+	private Stirng phoneNO; // 전화번호
+	Private int phoneBill; //전화요금
+	public int getPhoneBill() {
+		
+		
+		this("0000"); //같은 클래스의 다른 생성자를 호출
+		public void send(Stirng phoneNO)
+		
+	}
+}
+
+
+
+
+
+
+
+
+class smartphone extends Phone
+{
+	private int UsimId; // 유심 아이디
+	private int dataBill //데이타 요금
+	
+	
+}
+
+super(phoneNo);
+publicc
