38e4c664e056e176fb168f6adec5d70a14e98d92
Baek Gi Chan <11618nathan@gmail.com>
2019-02-26 18:28:57 +0900

    Upload

---
+++ DicProject/src/dic/Word.java
@@ -0,0 +1,44 @@
+package parking;
+
+
+public class CarInfo
+{
+	private int carNO; // 차량번호
+	private Date inputTime; //입차 시간 !!
+	private Stirng CarGrade;
+	
+	public int getCarNo {
+	public Data getinputTime() {
+	public String getCarGrade {
+		
+	}
+	}
+	}
+	
+}
+
+private CarInfo [][] parking; //주차공간 !!
+private int carCnt = 0;   // 주차 차량 대수 !!
+
+public void getCarCnt() {return carCnt; }
+
+public parking() {}
+//주차 공간 생성
+public parking
+
+private void settingparking()
+{
+	// 행?
+	
+	
+private int Row =0;
+private int <>
+}
+
+
+System.out.println("")
+
+
+
+
+
