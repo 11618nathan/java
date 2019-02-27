576f639ac5299da727bb0f88335e7e6f78f2ae2b
Baek Gi Chan <11618nathan@gmail.com>
2019-02-27 14:59:45 +0900

    Upload

---
+++ ex_package/src/test.java
@@ -0,0 +1,18 @@
+
+import Calc.minus.*;
+import Calc.plus.*;
+import ggu.*;
+
+
+public class test {
+
+	public static void main(String[] args) {
+		
+		Plus p =  new Plus(); 
+		Minus m = new Minus();
+		
+		
+		
+	}
+
+}
