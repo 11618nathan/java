f7f2a4874cbea109d51297c5d3c2de71ef6002d6
Baek Gi Chan <11618nathan@gmail.com>
2019-03-02 10:04:16 +0900

    Upload

---
+++ OOP/src/Student.java
@@ -0,0 +1,31 @@
+
+//	   속성 :  학번, 나이, 이름, 성적, 학과, 지도교수... 
+//	   행동 :  수업, 공부, 과제, 시험, 여행 
+public class Student
+{	// 속성 : 객체의 데이타들.. 
+	// 속성, 멤버 변수(C++), 필드 , 필드값 	
+	public int studentNo; 
+	public int studentAge; 
+	public String studentName; 
+	
+	// 행동 :  객체가 수행하는 기능들... 
+	// 행동, 기능, 펑션, 멤버함수(C++), 메소드(method)
+	public void Study() 
+	{
+		
+	}
+	public void Exm() 
+	{
+
+	}
+}
+/* C언어의 구조체 !! 객체가 아니다 !! 
+   속성만 정의 할수 있다... 
+   객체 = 속성 + 행동 !
+
+struct Student
+{
+	int studentNo;	
+	int studentAge; 
+	String studentName;
+}*/
