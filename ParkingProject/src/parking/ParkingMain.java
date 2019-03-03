b229cf01b7f00fccff052e0e32118b6de91a0e3d
Baek Gi Chan <11618nathan@gmail.com>
2019-03-03 18:56:38 +0900

    Upload

---
+++ ParkingProject/src/parking/ParkingMain.java
@@ -0,0 +1,12 @@
+package parking;
+
+public class ParkingMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Parking p =  new Parking();
+		p.settingParking();
+		p.show();
+	}
+
+}
