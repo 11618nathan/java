f0f0f89df40fc81096dd4514703868ccdb573821
Baek Gi Chan <11618nathan@gmail.com>
2019-03-03 18:55:46 +0900

    Upload

---
+++ ParkingProject/src/parking/CarGrade.java
@@ -0,0 +1,11 @@
+package parking;
+
+public class CarGrade
+{ 
+	private CarGrade(){}
+
+	public static final String SMALL_CAR = "경차";
+	public static final String VIP_CAR = "정기권";
+	public static final String NORMAL_CAR = "경차";
+	
+}
