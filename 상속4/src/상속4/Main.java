919095463f05365f31ca073f205106f40a8f912e
Baek Gi Chan <11618nathan@gmail.com>
2019-03-06 17:49:14 +0900

    Upload

---
+++ 상속4/src/상속4/Main.java
@@ -0,0 +1,100 @@
+// 추상 클래스 !!
+// - 1개 이상의 추상 메소드를 갖는 클래스!!
+
+
+
+
+
+
+
+
+
+class EMP
+{
+	private int empNO;
+	
+	public int getEmpno() {return empNo;}
+	
+	public Emp { }
+    public Emp(int empNo) {this.empNO = empNo; }
+    //추상메스드!!
+    // -> 부모에서 구햔할순 없지만... 부모의 자식을 반드시 갖어야할 함수 !!
+    public abstract void getSalary();
+	}
+}
+
+
+
+
+
+
+
+
+class NormalEmp extends Emp
+{
+	private int salary // 연봉 !!
+	public int getSalary() {this.salary =salary; }
+	
+	public NormalEmp(int empNo, int salary)
+	
+	public void getSalary() {System.out.println(salary/13); }
+	
+	class
+}
+@override
+public void getSalary 
+
+
+
+
+
+
+class Company // 사원 관리 (회사)
+{
+	private NormalEmps = new NormalEmp[10]
+			
+
+	
+    // 알바 사원 월급주기!!
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
